26171444ecd7dd857a0adf578c75c9618b35a4fe
kenzo03 <kevinwijaya480@gmail.com>
2022-03-06 22:12:49 +0700

    feature: add grid view for main screen if screen width > 600

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,43 +8,101 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Wisata Bandung')),
-        body: ListView.builder(
-          itemCount: tourismPlaceList.length,
-          itemBuilder: (context, index) {
-            final TourismPlace place = tourismPlaceList[index];
-
-            return InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return DetailScreen(place: place);
-                  }));
-                },
-                child: Card(
-                    child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Hero(
-                            tag: 'img', child: Image.asset(place.imageAsset)),
-                      ),
-                      Expanded(
-                          flex: 2,
-                          child: Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(place.name,
-                                      style: const TextStyle(fontSize: 16)),
-                                  const SizedBox(height: 10),
-                                  Text(place.location)
-                                ],
-                              ))),
-                    ])));
+        appBar: AppBar(
+            title: Text(
+                'Wisata Bandung. Size: ${MediaQuery.of(context).size.width}')),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth <= 600) {
+              return const TourismPlaceList();
+            } else {
+              return const TourismPlaceGrid();
+            }
           },
         ));
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  const TourismPlaceGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(24),
+        child: GridView.count(
+            crossAxisCount: 4,
+            children: tourismPlaceList.map((place) {
+              return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return DetailScreen(place: place);
+                    }));
+                  },
+                  child: Card(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                          child:
+                              Image.asset(place.imageAsset, fit: BoxFit.cover)),
+                      const SizedBox(height: 8),
+                      Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text(place.name,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold))),
+                      Padding(
+                          padding: const EdgeInsets.only(left: 8, bottom: 8),
+                          child: Text(place.location))
+                    ],
+                  )));
+            }).toList()));
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: tourismPlaceList.length,
+      itemBuilder: (context, index) {
+        final TourismPlace place = tourismPlaceList[index];
+
+        return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place: place);
+              }));
+            },
+            child: Card(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child:
+                        Hero(tag: 'img', child: Image.asset(place.imageAsset)),
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(place.name,
+                                  style: const TextStyle(fontSize: 16)),
+                              const SizedBox(height: 10),
+                              Text(place.location)
+                            ],
+                          ))),
+                ])));
+      },
+    );
   }
 }

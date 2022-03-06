856323fe0582887c6076c91fca2a1b749994ac3b
kenzo03 <kevinwijaya480@gmail.com>
2022-03-06 21:05:06 +0700

    feature: pass place parameter from main screen to detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,95 +1,86 @@
+import 'package:codelab1_tempat_wisata/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final TourismPlace place;
+
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(title: Text('Lembang Farm House')),
-        body: SafeArea(
-            child: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <
-                  Widget>[
-            Image.asset('images/farm-house.jpg'),
-            Container(
-                margin: EdgeInsets.only(top: 16),
-                child: Text('Farm House Lembang',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 30, fontFamily: 'Staatliches'))),
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 16),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        children: [
-                          Icon(Icons.calendar_today),
-                          SizedBox(height: 8),
-                          Text(
-                            'Open Everyday',
-                            style: informationTextStyle,
+        body: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Image.asset(place.imageAsset),
+                Container(
+                    margin: EdgeInsets.only(top: 16),
+                    child: Text(place.name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 30, fontFamily: 'Staatliches'))),
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Icon(Icons.calendar_today),
+                              SizedBox(height: 8),
+                              Text(
+                                place.openDays,
+                                style: informationTextStyle,
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Icon(Icons.access_time),
+                              SizedBox(height: 8),
+                              Text(
+                                place.openTime,
+                                style: informationTextStyle,
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Icon(Icons.monetization_on),
+                              SizedBox(height: 8),
+                              Text(
+                                place.ticketPrice,
+                                style: informationTextStyle,
+                              )
+                            ],
                           )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.access_time),
-                          SizedBox(height: 8),
-                          Text(
-                            '09:00 - 20:00',
-                            style: informationTextStyle,
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.monetization_on),
-                          SizedBox(height: 8),
-                          Text(
-                            'RP 25.000',
-                            style: informationTextStyle,
-                          )
-                        ],
-                      )
-                    ])),
-            Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                    ))),
-            Container(
-                height: 150,
-                child: ListView(scrollDirection: Axis.horizontal, children: [
-                  Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                      )),
-                  Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                      )),
-                  Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
-                      )),
-                ])),
-          ]),
-        )));
+                        ])),
+                Container(
+                    padding: EdgeInsets.all(16),
+                    child: Text(place.description,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 16, fontFamily: 'Oxygen'))),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: place.imageUrls.map((url) {
+                      return Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(url),
+                          ));
+                    }).toList(),
+                  ),
+                ),
+              ]),
+        ));
   }
 }

81482ded9fe330b36f729b95d50251d783003dec
kenzo03 <kevinwijaya480@gmail.com>
2022-03-06 21:14:10 +0700

    feature: add back button in detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -11,76 +11,87 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(title: Text('Lembang Farm House')),
         body: SingleChildScrollView(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Image.asset(place.imageAsset),
-                Container(
-                    margin: EdgeInsets.only(top: 16),
-                    child: Text(place.name,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 30, fontFamily: 'Staatliches'))),
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 16),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Column(
-                            children: [
-                              Icon(Icons.calendar_today),
-                              SizedBox(height: 8),
-                              Text(
-                                place.openDays,
-                                style: informationTextStyle,
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Icon(Icons.access_time),
-                              SizedBox(height: 8),
-                              Text(
-                                place.openTime,
-                                style: informationTextStyle,
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Icon(Icons.monetization_on),
-                              SizedBox(height: 8),
-                              Text(
-                                place.ticketPrice,
-                                style: informationTextStyle,
-                              )
-                            ],
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(children: <Widget>[
+              Image.asset(place.imageAsset),
+              SafeArea(
+                child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: IconButton(
+                          icon: Icon(Icons.arrow_back, color: Colors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    )),
+              )
+            ]),
+            Container(
+                margin: EdgeInsets.only(top: 16),
+                child: Text(place.name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30, fontFamily: 'Staatliches'))),
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          Icon(Icons.calendar_today),
+                          SizedBox(height: 8),
+                          Text(
+                            place.openDays,
+                            style: informationTextStyle,
                           )
-                        ])),
-                Container(
-                    padding: EdgeInsets.all(16),
-                    child: Text(place.description,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            fontSize: 16, fontFamily: 'Oxygen'))),
-                Container(
-                  height: 150,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: place.imageUrls.map((url) {
-                      return Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(url),
-                          ));
-                    }).toList(),
-                  ),
-                ),
-              ]),
-        ));
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.access_time),
+                          SizedBox(height: 8),
+                          Text(
+                            place.openTime,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.monetization_on),
+                          SizedBox(height: 8),
+                          Text(
+                            place.ticketPrice,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      )
+                    ])),
+            Container(
+                padding: EdgeInsets.all(16),
+                child: Text(place.description,
+                    textAlign: TextAlign.center,
+                    style:
+                        const TextStyle(fontSize: 16, fontFamily: 'Oxygen'))),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(url),
+                      ));
+                }).toList(),
+              ),
+            ),
+          ]),
+    ));
   }
 }

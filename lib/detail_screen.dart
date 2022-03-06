c51441d6693b23889282d36b545fcfb3208ef9a6
kenzo03 <kevinwijaya480@gmail.com>
2022-03-06 19:55:04 +0700

    feature: add destination image and list images

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -7,9 +7,11 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
+            child: SingleChildScrollView(
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
             Container(
                 margin: EdgeInsets.only(top: 16),
                 child: Text('Farm House Lembang',
@@ -51,7 +53,32 @@ class DetailScreen extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 16,
                     ))),
+            Container(
+                height: 150,
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      )),
+                ])),
           ]),
-    ));
+    )));
   }
 }
